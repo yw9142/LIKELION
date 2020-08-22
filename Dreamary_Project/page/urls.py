039f04d7b38cb1ae8a5275fc4ab6081af2dcc337
Yonpark <yw9142@gmail.com>
2020-08-22 15:47:39 +0900

    CRUD complete

--- Dreamary_Project/page/urls.py
+++ Dreamary_Project/page/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('introduce/', views.introduce, name="introduce"),
-    path('profile/<int:designer_id>', views.detail, name="detail")
+    path('profile/<int:designer_id>', views.detail, name="detail"),
+    path('new/', views.new, name="new"),
+    path('create/', views.create, name="create"),
+    path('delete/<int:designer_id>/', views.delete, name="delete"),
+    path('update/<int:designer_id>/', views.update, name="update"),
 ]
--- Dreamary_Project/page/views.py
+++ Dreamary_Project/page/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Designer
 
 
@@ -15,3 +15,44 @@ def introduce(request):
 def detail(request, designer_id):
     designer = get_object_or_404(Designer, pk=designer_id)
     return render(request, 'detail.html', {'designer': designer})
+
+
+def new(request):
+    return render(request, 'new.html')
+
+
+def create(request):
+    if request.method == 'POST':
+        post = Designer()
+        if 'image' in request.FILES:
+            post.Image = request.FILES['image']
+        post.name = request.POST['name']
+        post.address = request.POST['address']
+        post.description = request.POST['description']
+
+        post.save()
+
+        return redirect('detail', post.id)
+
+
+def delete(request, designer_id):
+    post = get_object_or_404(Designer, pk=designer_id)
+    post.delete()
+
+    return redirect('home')
+
+
+def update(request, designer_id):
+    post = get_object_or_404(Designer, pk=designer_id)
+
+    if request.method == 'POST':
+        if 'image' in request.FILES:
+            post.Image = request.FILES['image']
+        post.name = request.POST['name']
+        post.address = request.POST['address']
+        post.description = request.POST['description']
+
+        post.save()
+        return redirect('detail', post.id)
+    else:
+        return render(request, 'update.html', {'designer': post})
