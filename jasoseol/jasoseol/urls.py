85a801bd9f9bfa0e280d4deb91de21bd954b8715
Yonpark <yw9142@gmail.com>
2020-09-18 21:44:56 +0900

    Jasoseol.com CRUD

--- jasoseol/jasoseol/urls.py
+++ jasoseol/jasoseol/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main.views import index, create, detail, delete, update
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name="index"),
+    path('create/', create, name="create"),
+    path('detail/<int:jss_id>', detail, name="detail"),
+    path('delete/<int:jss_id>', delete, name="delete"),
+    path('update/<int:jss_id>', update, name="update"),
 ]
