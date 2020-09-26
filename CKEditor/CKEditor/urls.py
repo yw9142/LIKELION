3c3e552c3e4ab7c170c3c54091f52f6c98a78b8e
Yonpark <yw9142@gmail.com>
2020-09-26 23:00:16 +0900

    Add CKEditor example

---
+++ CKEditor/CKEditor/urls.py
@@ -0,0 +1,30 @@
+"""CKEditor URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
+import blogapp.views
+from django.conf.urls import include  # Add
+from django.conf import settings  # Add
+from django.conf.urls.static import static  # Add
+
+urlpatterns = [
+                  path('admin/', admin.site.urls),
+                  path('', blogapp.views.index, name='index'),
+                  path('blogMain/', blogapp.views.blogMain, name='blogMain'),
+                  path('blogMain/createBlog/', blogapp.views.createBlog, name='createBlog'),
+                  path('ckeditor/', include('ckeditor_uploader.urls')),  # Add
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Add
---
+++ CKEditor/README.md
@@ -0,0 +1,10 @@
+# CKEditor
+
+#### 1. Cover Page
+![CoverPage](README_Images/Main.PNG)
+
+#### 2. CreateForm
+![CreateFrom](README_Images/CreateBlog_2.PNG)
+
+#### 3. Blog Main
+![BlogMain](README_Images/BlogMain.PNG)
